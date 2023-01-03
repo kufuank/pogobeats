dc6a91e2d8ca7657ed263244e2e88879a01a2e2d
kufu <62999569+kufuank@users.noreply.github.com>
2023-01-03 21:23:33 +0300

    ses deneme ve logo ekli

---
+++ Assets/playsound.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+using System.Collections;
+
+public class playsound : MonoBehaviour
+{
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
