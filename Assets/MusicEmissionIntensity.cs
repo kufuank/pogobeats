fabd6a25afbf0ae76393da567d610da8e52ed742
kufu <62999569+kufuank@users.noreply.github.com>
2023-02-06 20:54:52 +0300

    palmiyeler ve emisyon sese baglanmis

---
+++ Assets/MusicEmissionIntensity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicEmissionIntensity : MonoBehaviour
+{
+    public Material material;
+    public AudioSource audioSource;
+    public float emissionMultiplier = 1;
+
+    private float[] samples = new float[512];
+
+    private void Update()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+        float intensity = 0;
+        for (int i = 0; i < samples.Length; i++)
+        {
+            intensity += samples[i];
+        }
+        intensity = intensity / samples.Length * emissionMultiplier;
+        material.SetColor("_EmissionColor", new Color(intensity, intensity, intensity));
+    }
+}
